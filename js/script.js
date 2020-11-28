3b4853f7fda44ca28c606af8e9cb77a5bb264222
Enrico Borges <enricolbc@al.insper.edu.br>
2020-11-28 17:44:24 -0300

    asdas

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ function myFunction() {
     var x = document.getElementById("myTextarea").value;
     document.getElementById("myProgress").value = x;
   }
+
+
+function estudar() {
+  document.getElementById("demo").innerHTML = "Status:  Se concentre hora de estudar";
+  document.getElementById('myButton1').className = 'formatg';
+  document.getElementById('myButton2').className = 'formatr';
+  btn.setBackground(Color.BLACK);
+
+}
+
+function jogar() {
+  document.getElementById("demo").innerHTML = "Status:  Hora de se divertir !!";
+  document.getElementById('myButton2').className = 'formatg';
+  document.getElementById('myButton1').className = 'formatr';
+}
